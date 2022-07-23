f75389fd8b1aaf2888867f2886b12aba7cce3dca
mujtaba <mujtabatarar@gmail.com>
2022-07-23 06:50:06 +0500

    this is first git commit of this app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld/>
 </template>
 
 <script>
@@ -22,5 +21,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+main {
+  background-size: cover;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,123 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <input
+      class="inputField"
+      type="text"
+      placeholder="Enter City"
+      v-model="city"
+      @keypress="fetchApi"
+    />
+    <h1 class="cityHeading">{{ city }}</h1>
+    <h2 class="dateClass">{{ date }}</h2>
+    <div class="blurBox">
+      <p class="tempText">{{ temperature }}</p>
+    </div>
+
+    <p class="overview">{{ overview }}</p>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data() {
+    return {
+      search: "lahore",
+      city: "lahore",
+      date: "",
+      temperature: "",
+      overview: "",
+      sunrise: "",
+      sunset: "",
+      lat: "",
+      lon: "",
+    };
+  },
+
+  async mounted() {},
+
+  methods: {
+    async fetchApi(e) {
+      console.warn("infetchApi");
+      if (e.key == "Enter") {
+        console.log("enterPressed");
+        const locationResults = await axios.get(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${this.city}&limit=5&appid=9568693bb5d99cd9e7a11079bd670592`
+        );
+
+        this.lat = locationResults.data[0].lat;
+        this.lon = locationResults.data[0].lon;
+
+        console.warn(locationResults.data[0].lat);
+        const weatherResutls = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=9568693bb5d99cd9e7a11079bd670592`
+        );
+        console.warn(weatherResutls.data);
+        this.city = weatherResutls.data.name;
+        let today = new Date();
+        this.date = today.toDateString();
+        this.temperature = parseInt(weatherResutls.data.main.temp);
+        this.temperature = this.temperature - 274.15;
+        this.temperature = this.temperature.toPrecision(3);
+        this.temperature = this.temperature + "c";
+        this.overview = weatherResutls.data.weather[0].main;
+        console.warn("oveeeer" + this.overview);
+        this.sunrise = weatherResutls.data.sys.sunrise;
+        this.sunset = weatherResutls.data.sys.sunset;
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.inputField {
+  font-size: 18pt;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  border-radius: 5px 50px 5px;
+  border: 2px solid #609;
+  text-align: center;
+  padding: 20px;
+  width: 60vw;
+  height: 15px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.cityHeading {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: azure;
+  font-size: 32pt;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.tempText {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  color: chartreuse;
+  font-size: 52pt;
 }
-a {
-  color: #42b983;
+.blurBox {
+  -webkit-backdrop-filter: blur(5px);
+  backdrop-filter: blur(5px);
+  background: rgba(239, 233, 233, 0.043);
+  border-radius: 20px;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 200px;
+  max-height: 250px;
+  min-width: 150px;
+  max-width: 250px;
+margin: 0 auto;
+  
+}
+.dateClass {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: aliceblue;
+}
+.overview {
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+  color: aliceblue;
+  font-size: 32px;
+  font-style: italic;
 }
 </style>
